02eb382331f7541e6992b1747a412e372dbcd9e5
Trey <104473511+Lemonns@users.noreply.github.com>
2022-08-25 21:20:08 -0500

    Changed how pages get reset

--- src/index.js
+++ src/index.js
@@ -2,47 +2,38 @@ import './style.css';
 import {createHeader, renderHomeScreen} from './home.js'
 import {createMenuTitle, createMenu} from './menu.js'
 import {createContactForm} from './contact.js'
+
 const mainContainer = document.querySelector('.content')
+const secondaryContainer = document.createElement('div')
+
+secondaryContainer.classList.add('secondary-content')
 
 createHeader(mainContainer)
-renderHomeScreen(mainContainer)
-createMenuTitle(mainContainer)
-createMenu(mainContainer)
-createContactForm(mainContainer)
-
-const menuTitle = document.querySelector('.title')
-const menu = document.querySelector('.menu-container')
-const contactForm = document.querySelector('.contact-container')
-menu.style.display = 'none'
-menuTitle.style.display = 'none'
-contactForm.style.display = 'none'
-
+mainContainer.appendChild(secondaryContainer)
+renderHomeScreen(secondaryContainer)
 
 const menuButton = document.querySelector('.menu')
 const homeButton = document.querySelector('.home')
 const contactButton = document.querySelector('.contact')
 
 menuButton.addEventListener('click', () => {
-    const homeScreen = document.querySelector('.card-container')
-    contactForm.style.display = 'none'
-    homeScreen.style.display = 'none'
-    menu.style.display = 'flex'
-    menuTitle.style.display = 'flex'
+    clearContent(secondaryContainer)
+    createMenuTitle(secondaryContainer)
+    createMenu(secondaryContainer)
 })
 
 contactButton.addEventListener('click', () => {
-    const homeScreen = document.querySelector('.card-container')
-    menu.style.display = 'none'
-    menuTitle.style.display = 'none'
-    homeScreen.style.display = 'none'
-    contactForm.style.display = 'flex'
+    clearContent(secondaryContainer)
+    createContactForm(secondaryContainer)
 })
 
 
 homeButton.addEventListener('click', () => {
-    const homeScreen = document.querySelector('.card-container')
-    homeScreen.style.display = 'flex'
-    menu.style.display = 'none'
-    menuTitle.style.display = 'none'
-    contactForm.style.display = 'none'
+    clearContent(secondaryContainer)
+    renderHomeScreen(secondaryContainer)
 })
+
+
+function clearContent(contentContainer) {
+    contentContainer.innerHTML = "";
+}
